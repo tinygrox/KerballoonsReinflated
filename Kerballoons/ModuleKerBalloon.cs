32d6ebf67851d6730b6d1007da82a2870bfdaa5e
Jonathan Bayer <linuxgurugamer@gmail.com>
2021-03-04 14:03:17 -0500

    Test build changing part.AddForceAtPosition to part.Rigidbody.AddForceAtPosition, needed for Principia

--- Kerballoons/ModuleKerBalloon.cs
+++ Kerballoons/ModuleKerBalloon.cs
@@ -426,7 +426,7 @@ namespace KerBalloons
                     if (isInflated)
                     {
                         float lift = BalloonProperties.getLift(this);
-                        this.part.AddForceAtPosition(vessel.upAxis * lift, liftPointObject.transform.position);
+                        this.part.Rigidbody.AddForceAtPosition(vessel.upAxis * lift, liftPointObject.transform.position);
                         Vector3 scale = new Vector3(BalloonProperties.getScale(this), BalloonProperties.getScale(this), BalloonProperties.getScale(this));
                         balloonObject.transform.localScale = scale;
 
@@ -447,7 +447,7 @@ namespace KerBalloons
                             float progress = scaleInc / BalloonProperties.getScale(this);
 
                             float lift = BalloonProperties.getLift(this) * progress;
-                            this.part.AddForceAtPosition(vessel.upAxis * lift, liftPointObject.transform.position);
+                            this.part.Rigidbody.AddForceAtPosition(vessel.upAxis * lift, liftPointObject.transform.position);
 
                             ropeObject.transform.localScale = new Vector3(1, 1, progress);
                             balloonObject.transform.position = balloonPointObject.transform.position;
@@ -477,7 +477,7 @@ namespace KerBalloons
                         float progress = scaleInc / BalloonProperties.getScale(this);
 
                         float lift = BalloonProperties.getLift(this) * progress;
-                        this.part.AddForceAtPosition(vessel.upAxis * lift, liftPointObject.transform.position);
+                        this.part.Rigidbody.AddForceAtPosition(vessel.upAxis * lift, liftPointObject.transform.position);
 
 
                         ropeObject.transform.rotation = Quaternion.Slerp(ropeObject.transform.rotation, Quaternion.LookRotation(vessel.upAxis, vessel.upAxis), BalloonProperties.getLift(this) / 10);
